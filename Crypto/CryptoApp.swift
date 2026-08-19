3b35e6d4a76547548fb5ec3de137f657f2b7d62b
kareemelazab <kareem.ahmed.moniem@gmail.com>
2026-08-19 11:35:59 +0300

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Kareem on 19/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Crypto/Extensions/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Crypto
+//
+//  Created by Kareem on 19/08/2026.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+}
+
+
