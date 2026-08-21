9bc3839c307e41fc38b6f7b410b06528fcd15ca0
kareemelazab <kareem.ahmed.moniem@gmail.com>
2026-08-21 09:20:00 +0300

    fix: rename GreenColor,RedColor to themeGreen,themeRed

--- Crypto/Extensions/Color.swift
+++ Crypto/Extensions/Color.swift
@@ -15,8 +15,8 @@ extension Color {
 struct ColorTheme {
     let accent = Color("AccentColor")
     let background = Color("BackgroundColor")
-    let green = Color("GreenColor")
-    let red = Color("RedColor")
+    let green = Color("themeGreen")
+    let red = Color("themeRed")
     let secondaryText = Color("SecondaryTextColor")
 }
 
