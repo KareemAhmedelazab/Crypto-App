1b89537db05ce1b612bceecc39312c6e4ac490f2
kareemelazab <kareem.ahmed.moniem@gmail.com>
2026-08-22 15:44:50 +0300

    refactor: add func to handle custom errors

--- Crypto/Utilities/NetworkingManger.swift
+++ Crypto/Utilities/NetworkingManger.swift
@@ -10,20 +10,33 @@ import Combine
 
 class NetworkingManger {
     
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponse(url: let url): return "bad response from URL. \(url)"
+            case .unknown: return "unknown error occured."
+            }
+        }
+    }
+    
     static func download(url: URL) -> AnyPublisher<Data, any Error> {
         return URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { (output) -> Data in
-                
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
+            .tryMap({ try HandleURLResponse(output: $0,url: url)})
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
 
+    }
+    
+    static func HandleURLResponse(output: URLSession.DataTaskPublisher.Output,url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {
+            throw NetworkingError.badURLResponse(url: url)
+        }
+        return output.data
     }
     
     static func handleCompletion(completion: Subscribers.Completion<Error>) {
