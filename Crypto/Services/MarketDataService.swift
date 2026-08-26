3e2f9e839409a147e152fe6a0f797e5e8baff99c
kareemelazab <kareem.ahmed.moniem@gmail.com>
2026-08-26 16:19:40 +0300

    feat: create MarketDataService
    get MarketData from coingecko

---
+++ Crypto/Services/MarketDataService.swift
@@ -0,0 +1,31 @@
+//
+//  MarketDataService.swift
+//  Crypto
+//
+//  Created by Kareem on 26/08/2026.
+//
+
+import Foundation
+import Combine
+
+class MarketDataService {
+    
+    @Published var marketData: MarketDataModel? = nil
+    var marketSubscription: AnyCancellable?
+    
+    init() {
+        getData()
+    }
+    
+    private func getData() {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else { return }
+        
+        marketSubscription = NetworkingManger.download(url: url)
+            .decode(type: GlobelData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManger.handleCompletion(completion:), receiveValue: { [weak self] returnedGlobelData in
+                self?.marketData = returnedGlobelData.data
+                self?.marketSubscription?.cancel()
+            })
+        
+    }
+}
