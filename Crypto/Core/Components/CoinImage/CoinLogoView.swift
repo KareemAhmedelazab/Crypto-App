ea38987be89192c87dca37fa760175d4465388b0
kareemelazab <kareem.ahmed.moniem@gmail.com>
2026-08-27 12:54:58 +0300

    feat: create CoinLogoView

---
+++ Crypto/Core/Components/CoinImage/CoinLogoView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinLogoView.swift
+//  Crypto
+//
+//  Created by Kareem on 27/08/2026.
+//
+
+import SwiftUI
+
+struct CoinLogoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinLogoView()
+}
