38ce404b2fecac30015f2997e10017f24ed7b22e
kareemelazab <kareem.ahmed.moniem@gmail.com>
2026-08-28 20:29:01 +0300

    feat: create DetailViewModel and CoinDetailDataService

---
+++ Crypto/Core/Detail/ViewModels/DetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewModel.swift
+//  Crypto
+//
+//  Created by Kareem on 28/08/2026.
+//
+
+import Foundation
---
+++ Crypto/Services/CoinDetailDataService.swift
@@ -0,0 +1,8 @@
+//
+//  CoinDetailDataService.swift
+//  Crypto
+//
+//  Created by Kareem on 28/08/2026.
+//
+
+import Foundation
