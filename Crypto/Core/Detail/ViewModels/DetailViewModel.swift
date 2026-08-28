298d474e7b49731dbed867d86dc678495ce64e3d
kareemelazab <kareem.ahmed.moniem@gmail.com>
2026-08-28 20:32:01 +0300

    feat: add DetailViewModel with Combine data subscribers

--- Crypto/Core/Detail/ViewModels/DetailViewModel.swift
+++ Crypto/Core/Detail/ViewModels/DetailViewModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import Combine
+
+class DetailViewModel: ObservableObject {
+    
+    private let coinDetailService: CoinDetailDataService
+    private var cancebllables = Set<AnyCancellable>()
+    
+    init(coin: CoinModel) {
+    
+        self.coinDetailService = CoinDetailDataService(coin: coin)
+        self.addSubscribers()
+    }
+    
+    private func addSubscribers() {
+        coinDetailService.$coinDetails
+            .sink { returnCoinDetail in
+                print("recieved coin detail data")
+                print(returnCoinDetail)
+            }
+            .store(in: &cancebllables)
+            
+    }
+}
