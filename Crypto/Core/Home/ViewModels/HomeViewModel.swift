6b0613a078e4a07637a14c19db24a68942217d14
kareemelazab <kareem.ahmed.moniem@gmail.com>
2026-08-28 14:47:42 +0300

    feat: add sorting logic

--- Crypto/Core/Home/ViewModels/HomeViewModel.swift
+++ Crypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -16,29 +16,28 @@ class HomeViewModel: ObservableObject {
     @Published var portfolioCoins: [CoinModel] = []
     @Published var searchText: String = ""
     @Published var isLoading: Bool = false
+    @Published var sortOption: SortOption = .holdings
     
     private let coinDataService = CoinDataService()
     private let marketDataService = MarketDataService()
     private let portfilioDataService = PortfolioDataService()
     private var cancellables = Set<AnyCancellable>()
     
+    enum SortOption {
+        case rank, rankReversed, holdings, holdingsReversed, price, priceReversed
+    }
+    
     init() {
         addSubscribers()
     }
     
     func addSubscribers() {
-        // old allCoins sink
-        /*        dataService.$allCoins
-         //            .sink { [weak self] returnedCoins in
-         //                self?.allCoins = returnedCoins
-         //            }
-         //            .store(in: &cancellables)
-         */
+        
         // updates allCoins
         $searchText
-            .combineLatest(coinDataService.$allCoins)
+            .combineLatest(coinDataService.$allCoins,$sortOption)
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
-            .map(filterCoins)
+            .map(filterAndSortCoins)
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
@@ -49,7 +48,8 @@ class HomeViewModel: ObservableObject {
             .combineLatest(portfilioDataService.$savedEntities)
             .map(mapAllCoinToPortfolioCoin)
             .sink { [weak self] returnedCoins in
-                self?.portfolioCoins = returnedCoins
+                guard let self else { return }
+                self.portfolioCoins = self.sortHoldingsCoin(coins: returnedCoins)
             }
             .store(in: &cancellables)
         
@@ -75,6 +75,13 @@ class HomeViewModel: ObservableObject {
         HapticManger.notification(type: .success)
     }
     
+    private func filterAndSortCoins(text: String, coins: [CoinModel],sort: SortOption) -> [CoinModel] {
+        var updatedCoins = filterCoins(text: text, coins: coins)
+        sortCoins(sort: sort, coins: &updatedCoins)
+        
+        return updatedCoins
+    }
+    
     private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
         guard !text.isEmpty else {
             return coins
@@ -87,6 +94,40 @@ class HomeViewModel: ObservableObject {
             coin.id.lowercased().contains(lowercasedText)
         }
         return filteredCoins
+    }
+    
+    private func sortCoins(sort: SortOption, coins: inout [CoinModel]) {
+        switch sort {
+        case .rank:
+                coins.sort { coin1, coin2 in
+                    let rank1 = coin1.rank == 0 ? Int.max : coin1.rank
+                    let rank2 = coin2.rank == 0 ? Int.max : coin2.rank
+                    return rank1 < rank2
+                }
+            case .rankReversed:
+                coins.sort { coin1, coin2 in
+                    let rank1 = coin1.rank == 0 ? Int.min : coin1.rank
+                    let rank2 = coin2.rank == 0 ? Int.min : coin2.rank
+                    return rank1 > rank2
+                }
+        case .holdings, .holdingsReversed:
+                 break
+        case .price:
+             coins.sort(by: { $0.currentPrice > $1.currentPrice })
+        case .priceReversed:
+             coins.sort(by: { $0.currentPrice < $1.currentPrice })
+        }
+    }
+    
+    private func sortHoldingsCoin(coins: [CoinModel]) -> [CoinModel] {
+        switch sortOption {
+        case .holdings:
+            return coins.sorted(by: { $0.currentHoldingValue > $1.currentHoldingValue})
+        case .holdingsReversed:
+            return coins.sorted(by: { $0.currentHoldingValue < $1.currentHoldingValue})
+        default:
+            return coins
+        }
     }
     
     private func mapAllCoinToPortfolioCoin(allCoins: [CoinModel], portfolioEntities: [PortfolioEntity]) -> [CoinModel] {
