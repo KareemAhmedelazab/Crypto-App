6db7c1dc39197858d9bb75e22249ac428cc35f3b
kareemelazab <kareem.ahmed.moniem@gmail.com>
2026-08-28 18:22:32 +0300

    feat: create DetailView

---
+++ Crypto/Core/Detail/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Crypto
+//
+//  Created by Kareem on 28/08/2026.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
