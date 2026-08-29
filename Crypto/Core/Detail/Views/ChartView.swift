3beed4d7176793d82b326c3eab62243a8a452d09
kareemelazab <kareem.ahmed.moniem@gmail.com>
2026-08-29 20:04:25 +0300

    feat: create ChartView

---
+++ Crypto/Core/Detail/Views/ChartView.swift
@@ -0,0 +1,82 @@
+//
+//  ChartView.swift
+//  Crypto
+//
+//  Created by Kareem on 29/08/2026.
+//
+
+// chart with Path + GeometryReader
+
+import SwiftUI
+
+struct ChartView: View {
+    
+    let data: [Double]
+    let maxY: Double
+    let minY: Double
+    let lineColor: Color
+    
+    init(coin: CoinModel) {
+        data = coin.sparklineIn7D?.price ?? []
+        maxY = data.max() ?? 0
+        minY = data.min() ?? 0
+        
+        let priceChange = (data.last ?? 0) - (data.first ?? 0)
+        lineColor = priceChange > 0 ? Color.theme.green : Color.theme.red
+        }
+    
+    var body: some View {
+        chartView
+            .frame(height: 200)
+            .background(chartBackground)
+            .overlay(alignment: .leading) {chartYAxis}
+    }
+}
+
+#Preview {
+    ChartView(coin: DeveloperPreview.instance.coin)
+}
+
+extension ChartView {
+    
+    private var chartView: some View {
+        
+        GeometryReader { geometry in
+            Path { path in
+                        for index in data.indices {
+                            let xPosition = geometry.size.width / CGFloat(data.count) * CGFloat(index + 1)
+                            let yAxis = maxY - minY
+                            
+                            let yPosition = ( 1 - CGFloat((data[index] - minY) / yAxis)) * geometry.size.height
+                            
+                            if index == 0 {
+                                path.move(to: CGPoint(x: xPosition, y: yPosition))
+                            }
+                            path.addLine(to: CGPoint(x: xPosition, y: yPosition))
+                        }
+                    }
+            .stroke(lineColor, style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
+        }
+    }
+    
+    private var chartBackground: some View {
+        VStack{
+            Divider()
+            Spacer()
+            Divider()
+            Spacer()
+            Divider()
+        }
+    }
+    
+    private var chartYAxis: some View {
+        
+        VStack {
+            Text(maxY.formattedWithAbbreviations())
+            Spacer()
+            Text(((maxY + minY) / 2).formattedWithAbbreviations())
+            Spacer()
+            Text(minY.formattedWithAbbreviations())
+        }
+    }
+}
