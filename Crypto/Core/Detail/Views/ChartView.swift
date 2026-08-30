485155b691cc49955afee427110616a53c0db0d3
kareemelazab <kareem.ahmed.moniem@gmail.com>
2026-08-30 06:26:56 +0300

    feat: create ChartView useing geometry,Path

--- Crypto/Core/Detail/Views/ChartView.swift
+++ Crypto/Core/Detail/Views/ChartView.swift
@@ -11,10 +11,13 @@ import SwiftUI
 
 struct ChartView: View {
     
-    let data: [Double]
-    let maxY: Double
-    let minY: Double
-    let lineColor: Color
+    private let data: [Double]
+    private let maxY: Double
+    private let minY: Double
+    private let lineColor: Color
+    private let startingDate: Date
+    private let endingDate: Date
+    @State private var percentage: CGFloat = 0
     
     init(coin: CoinModel) {
         data = coin.sparklineIn7D?.price ?? []
@@ -23,13 +26,28 @@ struct ChartView: View {
         
         let priceChange = (data.last ?? 0) - (data.first ?? 0)
         lineColor = priceChange > 0 ? Color.theme.green : Color.theme.red
+        
+        endingDate = Date(coinGeckoString: coin.lastUpdated ?? "")
+        startingDate = endingDate.addingTimeInterval(-7*24*60*60)
         }
     
     var body: some View {
-        chartView
-            .frame(height: 200)
-            .background(chartBackground)
-            .overlay(alignment: .leading) {chartYAxis}
+        VStack {
+            chartView
+                .frame(height: 200)
+                .background(chartBackground)
+                .overlay(alignment: .leading) {chartYAxis.padding(.horizontal, 4)}
+            chartDateLabel.padding(.horizontal, 4)
+        }
+        .font(.caption)
+        .foregroundStyle(Color.theme.secondaryText)
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                withAnimation(.linear(duration: 2)) {
+                    percentage = 1.0
+                }
+            })
+        }
     }
 }
 
@@ -55,7 +73,15 @@ extension ChartView {
                             path.addLine(to: CGPoint(x: xPosition, y: yPosition))
                         }
                     }
+            .trim(from: 0.0, to: percentage)
             .stroke(lineColor, style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
+            .shadow(color: lineColor, radius: 10, x: 0.0, y: 10)
+            .shadow(color: lineColor.opacity(0.5), radius: 10, x: 0.0, y: 20)
+            .shadow(color: lineColor.opacity(0.2), radius: 10, x: 0.0, y: 30)
+            .shadow(color: lineColor.opacity(0.1), radius: 10, x: 0.0, y: 40)
+
+
+
         }
     }
     
@@ -77,6 +103,15 @@ extension ChartView {
             Text(((maxY + minY) / 2).formattedWithAbbreviations())
             Spacer()
             Text(minY.formattedWithAbbreviations())
+        }
+    }
+    
+    private var chartDateLabel: some View {
+        
+        HStack {
+            Text(startingDate.asShortDateString())
+            Spacer()
+            Text(endingDate.asShortDateString())
         }
     }
 }
