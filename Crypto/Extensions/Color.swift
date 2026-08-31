df99765dbb047c8c6c31aa95fe4e919940b4a046
kareemelazab <kareem.ahmed.moniem@gmail.com>
2026-08-31 16:53:21 +0300

    feat: build launchView UI with animated loading text

--- Crypto/Extensions/Color.swift
+++ Crypto/Extensions/Color.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 extension Color {
     static let theme = ColorTheme()
+    static let launch = LaunchTheme()
 }
 
 struct ColorTheme {
@@ -18,6 +19,12 @@ struct ColorTheme {
     let green = Color("themeGreen")
     let red = Color("themeRed")
     let secondaryText = Color("SecondaryTextColor")
+}
+
+struct LaunchTheme {
+    
+    let accent = Color("LaunchAccentColor")
+    let background = Color("LaunchBackgroundColor")
 }
 
 
